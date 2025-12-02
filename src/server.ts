567b8b96c2916d2659ea4bbea398a4284ba36aab
Rodrigo Cândido <rodrigocan2024@gmail.com>
2025-12-02 19:11:31 -0300

    Explorando Arquitetura ARM e AMD

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import { fastify } from 'fastify'
-import { fastifyCors } from '@fastify/cors'
-import { uploadImageRoute } from './routes/upload-image'
-import { fastifyMultipart } from '@fastify/multipart'
+import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
+import { fastify } from "fastify";
+import { healthCheckRoute } from "./routes/health-check.ts";
+import { uploadImageRoute } from "./routes/upload-image";
 
-const server = fastify()
+const server = fastify();
 
 server.register(fastifyCors, {
-  origin: '*',
-})
+	origin: "*",
+});
 
-server.register(fastifyMultipart)
-server.register(uploadImageRoute)
+server.register(fastifyMultipart);
+server.register(uploadImageRoute);
+server.register(healthCheckRoute);
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!!!')
-})
+server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+	console.log("HTTP server running!!!");
+});
